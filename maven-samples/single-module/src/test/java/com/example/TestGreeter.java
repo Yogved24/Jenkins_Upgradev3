4d18a716d5738770a8f73c6db10306fd48d18dda
Jenkins <jenkins@jenkins-u20m.yo.com>
2022-06-14 18:23:58 +0530

    test code updates

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,12 +27,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
 
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
